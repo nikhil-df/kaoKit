d024864587e755aa3bc35e6fc6e9dd72b6358558
Nikhil <157636459+nikhil-df@users.noreply.github.com>
2026-09-03 21:05:04 +0530

    kaomoji card done

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,17 +1,19 @@
 import { useColors } from "@/constants/colors";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import data from "@/assets/data/kaomojis_with_ids.json";
+import UseMojiCard from "@/components/mojiCard";
 
 
 export default function HomeScreen() {
   const colors = useColors()
   return (
-    <View>
+    <View style ={[{backgroundColor: colors.background},styles.container]}>
       <Text style={styles.title}>Welcome 👋</Text>
-
-      <Text style={styles.subtitle}>
-        This is your home screen
-      </Text>
+      <FlatList
+          data={data.kamomojis}
+          keyExtractor={(item) => item.id}
+          renderItem={({item}) => <UseMojiCard data={item}/>}
+          />
     </View>
   );
 }
@@ -19,10 +21,6 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#fff",
-    padding: 20,
   },
 
   title: {
--- src/components/mojiCard.tsx
+++ src/components/mojiCard.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native"
+import { useColors } from "@/constants/colors"
+import { StyleSheet, Text, useWindowDimensions, View } from "react-native"
 
 type Kaomoji = 
     {
@@ -14,13 +15,52 @@ type UserCardProps = {
 
 
 export default function UseMojiCard({ data }: UserCardProps) {
+  const colors = useColors()
+  const { width } = useWindowDimensions();
+
+  const fontSizes = {
+    small: width * 0.04,
+    medium: width * 0.06,
+    large: width * 0.08,
+    huge: width * 0.12,
+  };
+
   return (
-    <View>
-      <Text>{data.id}</Text>
-      <Text>{data.kaomoji}</Text>
-      <Text>{data.category}</Text>
-      <Text>{data.subcategory}</Text>
+    <View style={[{backgroundColor : colors.card , borderBlockColor:colors.border} , styles.container]}>
+      <View>
+      <Text style={[{color:colors.primary , fontSize : fontSizes.large}, styles.kaomoji]}>{data.kaomoji}</Text>
+      </View>
+      <View style={styles.categoryContainer}>
+      <Text style={[{color:colors.textMuted, fontSize : fontSizes.small}, styles.subcatagory]}>{data.subcategory}</Text>
+      <Text style={[{color: colors.primary},styles.dot]}>•</Text>
+      <Text style={[{color:colors.textMuted, fontSize : fontSizes.small}, styles.catagory]}>{data.category}</Text>
+      </View>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container :{
+      padding:10,
+      margin: 10,
+      paddingBottom: 0,
+      borderRadius : 10,
+      borderWidth: 3
+  },
+  kaomoji :{
+    fontWeight : '900'
+  },
+  categoryContainer:{
+    flexDirection : "row",
+  },
+  subcatagory:{
+    alignSelf: "center"
+  },
+  dot:{
+    fontSize: 30,
+    paddingHorizontal: 5
+  },
+  catagory:{
+    alignSelf: "center"
+  },
+})

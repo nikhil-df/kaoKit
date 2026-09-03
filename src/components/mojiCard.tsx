fd365e85cffa84cb3a8b8f27ae678eabd29b0350
Nikhil <157636459+nikhil-df@users.noreply.github.com>
2026-09-03 21:56:01 +0530

    made moji card copy to clipboard on click

--- src/components/mojiCard.tsx
+++ src/components/mojiCard.tsx
@@ -1,8 +1,10 @@
 import { useColors } from "@/constants/colors"
-import { StyleSheet, Text, useWindowDimensions, View } from "react-native"
+import { Pressable, StyleSheet, Text, useWindowDimensions, View } from "react-native"
+import * as Clipboard from "expo-clipboard";
+import { useState } from "react";
 
-type Kaomoji = 
-    {
+type Kaomoji =
+  {
     id: string,
     kaomoji: string,
     subcategory: string,
@@ -10,13 +12,14 @@ type Kaomoji =
   }
 
 type UserCardProps = {
-    data : Kaomoji
+  data: Kaomoji
 }
 
 
 export default function UseMojiCard({ data }: UserCardProps) {
   const colors = useColors()
   const { width } = useWindowDimensions();
+  const [copied, setCopied] = useState(false);
 
   const fontSizes = {
     small: width * 0.04,
@@ -25,42 +28,86 @@ export default function UseMojiCard({ data }: UserCardProps) {
     huge: width * 0.12,
   };
 
+  const handleCopy = async () => {
+    await Clipboard.setStringAsync(data.kaomoji);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 500);
+  };
+
   return (
-    <View style={[{backgroundColor : colors.card , borderBlockColor:colors.border} , styles.container]}>
-      <View>
-      <Text style={[{color:colors.primary , fontSize : fontSizes.large}, styles.kaomoji]}>{data.kaomoji}</Text>
+    <Pressable style={[{ backgroundColor: colors.card, borderBlockColor: colors.border }, styles.container]}
+      onPress={handleCopy}
+    >
+      <View style={styles.kaomojiContainer}>
+        <Text
+          style={[
+            { color: colors.primary, fontSize: fontSizes.large },
+            styles.kaomoji,
+            copied && styles.hiddenKaomoji,
+          ]}
+        >
+          {data.kaomoji}
+        </Text>
+
+        {copied && (
+          <Text
+            style={[
+              { color: colors.primary, fontSize: fontSizes.medium },
+              styles.copiedText,
+            ]}
+          >
+            Copied!
+          </Text>
+        )}
       </View>
       <View style={styles.categoryContainer}>
-      <Text style={[{color:colors.textMuted, fontSize : fontSizes.small}, styles.subcatagory]}>{data.subcategory}</Text>
-      <Text style={[{color: colors.primary},styles.dot]}>•</Text>
-      <Text style={[{color:colors.textMuted, fontSize : fontSizes.small}, styles.catagory]}>{data.category}</Text>
+        <Text style={[{ color: colors.textMuted, fontSize: fontSizes.small }, styles.subcatagory]}>{data.subcategory}</Text>
+        <Text style={[{ color: colors.primary }, styles.dot]}>•</Text>
+        <Text style={[{ color: colors.textMuted, fontSize: fontSizes.small }, styles.catagory]}>{data.category}</Text>
       </View>
-    </View>
+    </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
-  container :{
-      padding:10,
-      margin: 10,
-      paddingBottom: 0,
-      borderRadius : 10,
-      borderWidth: 3
+  container: {
+    padding: 10,
+    paddingBottom: 0,
+    paddingHorizontal:20,
+    margin: 10,
+    borderRadius: 10,
+    borderWidth: 3
   },
-  kaomoji :{
-    fontWeight : '900'
+  kaomoji: {
+    fontWeight: '900'
   },
-  categoryContainer:{
-    flexDirection : "row",
+  categoryContainer: {
+    flexDirection: "row",
   },
-  subcatagory:{
+  subcatagory: {
     alignSelf: "center"
   },
-  dot:{
+  dot: {
     fontSize: 30,
     paddingHorizontal: 5
   },
-  catagory:{
+  catagory: {
     alignSelf: "center"
   },
+  kaomojiContainer: {
+  position: "relative",
+  justifyContent: "center",
+
+ },
+
+hiddenKaomoji: {
+  opacity: 0,
+},
+copiedText: {
+  fontWeight: "600",
+  position:"absolute"
+},
 })
